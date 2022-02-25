4f353a9af72b10e8ae1db1f4c0809ae7683b0506
Martin Cervantes <cervantes.martine@gmail.com>
2022-02-25 12:49:58 -0700

    Delete mini practice project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import { useState } from 'react'
-import AddUser from './Components/AddUser'
-import UsersList from './Components/UsersList'
-
 const App = () => {
-  const [users, addUser] = useState([])
-
-  const saveUser = (newUser) => {
-    addUser([...users, newUser])
-  }
-
   return (
     <div className="container">
-        <AddUser saveUser={saveUser} />
-
-        <UsersList users={users} />
+        sdfasf
     </div>
   )
 }
